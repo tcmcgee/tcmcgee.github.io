13d5c36ef2185b947b7ecca2f556604303cdfa0a
Tom McGee <tmmcgee13@aol.com>
2015-07-17 13:30:27 -0500

    changed timing, switched email

--- js/myjs.js
+++ js/myjs.js
@@ -6,22 +6,23 @@ function loadHeader()
 
 function showWidgets()
 {
-	$('.clickables').delay(7000).fadeIn('slow');
+	$('.clickables').delay(3000).fadeIn('slow');
 }
 
 function moveRight()
 {
 	$('.blinker').css('margin-left','-40px');
-	$('.t').delay(1000).fadeIn(100);
-	$('.o').delay(1500).fadeIn(100);
-	$('.m').delay(2200).fadeIn(100);
-	$('.space').delay(2700).fadeIn(100);
+	$('.t').delay(500).fadeIn(100);
+	$('.o').delay(800).fadeIn(100);
+	$('.m').delay(1200).fadeIn(100);
+	$('.space').delay(1300).fadeIn(100);
 	$('.blinker').css('margin-left','0px');
-	$('.m2').delay(4000).fadeIn(100);
-	$('.c').delay(4700).fadeIn(100);
-	$('.g').delay(5300).fadeIn(100);
-	$('.e').delay(6000).fadeIn(100);
-	$('.e2').delay(6300).fadeIn(100);
+	$('.m2').delay(1600).fadeIn(100);
+	$('.c').delay(1900).fadeIn(100);
+	$('.g').delay(2200).fadeIn(100);
+	$('.e').delay(2500).fadeIn(100);
+	$('.e2').delay(2600).fadeIn(100);
+	$('.filler').delay(2600).fadeOut(100);
 }
 function blinker(){
 $(".blinker").fadeOut(200).delay(100).fadeIn(200).fadeOut(200).delay(100).fadeIn(200).fadeOut(200).delay(100).fadeIn(200).fadeOut(200).delay(100).fadeIn(200).fadeOut(200).delay(100).fadeIn(200).fadeOut(200).delay(100).fadeIn(200).fadeOut(200).delay(100).fadeIn(200).fadeOut(200).delay(100).fadeIn(200).fadeOut(200).delay(100).fadeIn(200).fadeOut(200).delay(100).fadeIn(200).fadeOut(200).delay(100).fadeIn(200).fadeOut(200).delay(100).fadeIn(200).fadeOut(200).fadeIn(200).fadeOut(200).delay(100).fadeIn(200).fadeOut(200);
