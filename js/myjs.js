20b2ac624956bf0fe5d10316e06096cda020364e
Tom McGee <tmmcgee13@aol.com>
2015-07-20 15:50:37 -0500

    Refactoring name animation to setTimeOut with Append instead of seperate html elemnts for each char

--- js/myjs.js
+++ js/myjs.js
@@ -12,24 +12,24 @@ function showWidgets()
 
 function moveRight()
 {
-	$('.blinker').css('margin-left','-40px');
-	$('.t').delay(500).fadeIn(100);
-	$('.o').delay(800).fadeIn(100);
-	$('.m').delay(1200).fadeIn(100);
-	$('.space').delay(1300).fadeIn(100);
-	$('.blinker').css('margin-left','0px');
-	$('.m2').delay(1700).fadeIn(100);
-	$('.c').delay(2100).fadeIn(100);
-	$('.g').delay(2400).fadeIn(100);
-	$('.e').delay(2700).fadeIn(100);
-	$('.e2').delay(2900).fadeIn(100);
-	$('.filler').delay(2900).fadeOut(100);
-	
+	setTimeout("$('.name').append('T');",100);
+	setTimeout("$('.name').append('o');",400);
+	setTimeout("$('.name').append('m');",800);
+	setTimeout("$('.name').append(' ');",1200);
+	setTimeout("$('.name').append('M');",1400);
+	setTimeout("$('.name').append('c');",1900);
+	setTimeout("$('.name').append('G');",2200);
+	setTimeout("$('.name').append('e');",2500);
+	setTimeout("$('.name').append('e');",2700);
+	//$('.filler').delay(2900).fadeOut(100);
+
 }
 function blinker(){
 $(".blinker").fadeOut(200).delay(100).fadeIn(200).fadeOut(200).delay(100).fadeIn(200).fadeOut(200).delay(100).fadeIn(200).fadeOut(200).delay(100).fadeIn(200).fadeOut(200).delay(100).fadeIn(200).fadeOut(200).delay(100).fadeIn(200).fadeOut(200);
 }
 function animateName()
 {
+	var name = 'Tom McGee'
+
 	moveRight();
 }
