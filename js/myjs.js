f2089eefc96f0719c525e419bdbb56af09a456be
tcmcgee <tmmcgee13@gmail.com>
2015-10-07 14:43:09 -0500

    Added sound!

--- js/myjs.js
+++ js/myjs.js
@@ -10,17 +10,19 @@ function showWidgets()
 	$('.description').delay(3200).fadeIn('slow');
 }
 
+
+
 function moveRight()
 {
-	setTimeout("$('.name').append('T');",100);
-	setTimeout("$('.name').append('o');",400);
-	setTimeout("$('.name').append('m');",800);
-	setTimeout("$('.name').append(' ');",1200);
-	setTimeout("$('.name').append('M');",1400);
-	setTimeout("$('.name').append('c');",1900);
-	setTimeout("$('.name').append('G');",2200);
-	setTimeout("$('.name').append('e');",2500);
-	setTimeout("$('.name').append('e');",2700);
+	setTimeout("$('.name').append('T');document.getElementById('click_noise1').play();",100);
+	setTimeout("$('.name').append('o');document.getElementById('click_noise3').play();",400);
+	setTimeout("$('.name').append('m');document.getElementById('click_noise1').play();",800);
+	setTimeout("$('.name').append(' ');document.getElementById('click_noise2').play();",1200);
+	setTimeout("$('.name').append('M');document.getElementById('click_noise3').play();",1400);
+	setTimeout("$('.name').append('c');document.getElementById('click_noise1').play();",1900);
+	setTimeout("$('.name').append('G');document.getElementById('click_noise3').play();",2200);
+	setTimeout("$('.name').append('e');document.getElementById('click_noise4').play();",2500);
+	setTimeout("$('.name').append('e');document.getElementById('click_noise4').play();",2700);
 	//$('.filler').delay(2900).fadeOut(100);
 
 }
